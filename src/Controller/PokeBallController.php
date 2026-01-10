d9faba153a82379b9bca5f36d5142344604bd5e7
maxime.chemin59551 <maxime.chemin59551@gmail.com>
2026-01-10 10:36:20 +0000

    ajout trad

--- src/Controller/PokeBallController.php
+++ src/Controller/PokeBallController.php
@@ -4,18 +4,21 @@ namespace App\Controller;
 
 use App\Repository\PokeBallRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PokeBallController extends AbstractController
 {
     #[Route('/', name: 'app_pokeball_index')]
-    public function index(PokeBallRepository $pokeBallRepository): Response
+    public function index(PokeBallRepository $pokeBallRepository, Request $request): Response
     {
         $pokeBalls = $pokeBallRepository->findAll();
+        $locale = $request->getLocale();
 
         return $this->render('pokeball/index.html.twig', [
             'pokeBalls' => $pokeBalls,
+            'locale' => $locale,
         ]);
     }
 }
